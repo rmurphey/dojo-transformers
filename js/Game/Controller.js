a8e1930752244ca23aedf47e3b279a3ade06665f
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-02-21 19:52:11 -0500

    initial commit

---
+++ js/Game/Controller.js
@@ -0,0 +1,93 @@
+dojo.provide('Game.Controller');
+
+dojo.require('dijit._Widget');
+dojo.require('dojo.string');
+
+dojo.require('Game.Transformers.Autobot');
+dojo.require('Game.Transformers.Decepticon');
+
+(function(d) {
+	d.declare('Game.Controller', dijit._Widget, {
+		rate : 			100, 	// ms
+		teamTemplate : '<li><label for="${team}"># of ${team}</label><input id="${team}" /></li>',
+		
+		constructor : function(args) {
+			this.teams = args.teams || {
+				autobots : Game.Transformers.Autobot,
+				decepticons : Game.Transformers.Decepticon
+			};
+			
+			this.maxTeamSize = args.maxTeamSize || 5;
+		},
+		
+		postCreate : function() {
+			this.setupTeams();
+
+			d.connect('generate', 'click', this, 'userGenerated');
+			d.connect('random', 'click', this, 'randomGenerated');
+
+			d.subscribe('/game/new', this, 'generate');
+		},
+		
+		setupTeams : function() {
+			this.inputs = {};
+			this.teamNames = [];
+
+			for (var team in this.teams) {
+				if (!this.teams.hasOwnProperty(team)) { return; }
+
+				this.inputs[team] = d.byId(team) || 
+					d.place(
+						d.string.substitute(this.teamTemplate, { team : team }), 
+						d.query('ul', this.domNode)[0],
+						'first'
+					);
+				this.teamNames.push(team);
+				this[team] = [];
+			}
+		},
+
+		generate : function(config) {
+			this.reset();
+
+			d.forEach(this.teamNames, function(team) {
+				while (--config[team]) {
+					this[team].push(new this.teams[team]());
+				}
+			});
+		},
+		
+		reset : function() {
+			d.publish('/game/reset');
+			d.forEach(this.teamNames, function(team) {
+				this[team] = [];
+			}, this);
+		},
+
+		userGenerated : function(e) {
+			var config = {};
+
+			d.forEach(this.teamNames, function(team) {
+				config[team] = this.inputs[team].value;
+			}, this);
+			
+			d.publish('/game/new', [ config ]);
+		},
+
+		randomGenerated : function(e) {
+			var config = {};
+			d.forEach(this.teamNames, function(team) {
+				config[team] = this.randomTeamSize();
+			}, this);
+			
+			d.publish('/game/new', [ config ]);
+		}
+		
+		randomTeamSize : function() {
+			return Math.floor(Math.random() * this.maxTeamSize) + 1
+		}
+	});
+	
+})(dojo);
+
+Game.Controller = new Game.Controller({ rate: dojo.config.frameRate || 10 }, 'controller');
---
+++ js/Game/Transformers/_Transformer.js
@@ -0,0 +1,11 @@
+dojo.provide('Game.Transformers._Transformer');
+
+dojo.require('dijit._Widget');
+
+(function(d) {
+	d.declare('Game.Transformers._Transformer', null, {
+		postCreate : function() {
+			
+		}
+	});	
+})(dojo);
