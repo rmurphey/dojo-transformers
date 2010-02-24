bbb61e3f84a1ecd074b24587801dc84455e63052
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-02-24 00:15:20 -0500

    dealing with recursion error
    it seems you can't have two classes that inherit from
    dijit._Widget that both have the same method name ...
    when i removed the inheritance a recursion error went
    away that i traced back to the fact that two classes
    had the same method name ('_play')

--- js/Transformers/Bot.js
+++ js/Transformers/Bot.js
@@ -5,37 +5,36 @@ dojo.require('Transformers.Missile');
 dojo.require('Transformers.Gun');
 
 (function(d) {
-	d.declare('Transformers.Bot', [ dijit._Widget ], {
-		constructor : function(config) {
-			this.config = config;
-			this.health = config.maxHealth;
+	d.declare('Transformers.Bot', null, {
+		constructor : function(team) {
+			this.team = team;
+			this.health = team.maxHealth;
 			
 			if (!this.health) {
 				console.log('you created a dead bot -- it has no health');
 				return;
 			}
 			
-			d.subscribe('/game/new', this, 'destroy');
-			d.subscribe('/bots/play', this, '_play');
+			d.subscribe('/' + this.team.name + '/bots/play', this, '_play');
 			d.subscribe('/bots/status', this, '_sendStatus');
 			
 			this.missiles = [];
 			this.guns = [];
 
-			while (this.config.missiles--) {
+			while (this.team.missiles--) {
 				this.missiles.push(new Transformers.Missile());
 			}
 			
-			while (this.config.guns--) {
+			while (this.team.guns--) {
 				this.guns.push(new Transformers.Gun());
 			}
 			
-			d.publish('/' + this.config.name + '/bot/join', [ this ]);
+			d.publish('/' + this.team.name + '/bot/join', [ this ]);
 		},
 		
 		_play : function(orders) {
-			console.log('orders', orders);
-			d.publish('/' + this.config.name + '/bot/ping', [ this ]);
+			console.log('playing');
+			d.publish('/' + this.team.name + '/bot/ping', [ this ]);
 		},
 		
 		_addHealth : function(h) {
